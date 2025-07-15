056419dabd91eda1c9674fbf12653608f201a72d
Rostislav Ivanov <x1qeee25@gmail.com>
2025-07-15 23:25:50 +0300

    Обновление POM и добавление компонентов

--- tests/test_courses.py
+++ tests/test_courses.py
@@ -14,7 +14,6 @@ def test_empty_courses_list(courses_list_page: CoursesListPage):
     courses_list_page.check_visible_empty_view()
 
 
-
 @pytest.mark.courses
 @pytest.mark.regression
 def test_create_course(chromium_page_with_state, create_course_page, courses_list_page):
@@ -22,21 +21,16 @@ def test_create_course(chromium_page_with_state, create_course_page, courses_lis
 
     create_course_page.check_visible_create_course_title()
     create_course_page.check_disabled_create_course_button()
-
-    create_course_page.check_visible_image_preview_empty_view()
-    create_course_page.check_visible_image_upload_view()
-
+    create_course_page.image_upload_widget.check_visible(is_image_uploaded=False)
     create_course_page.check_visible_create_course_form(
         title="", estimated_time="", description="", max_score="0", min_score="0")
 
     create_course_page.check_visible_exercises_title()
     create_course_page.check_visible_create_exercise_button()
-
     create_course_page.check_visible_exercises_empty_view()
 
-    create_course_page.upload_preview_image("./testdata/files/image.png")
-    create_course_page.check_visible_image_upload_view()
-
+    create_course_page.image_upload_widget.upload_preview_image("./testdata/files/image.png")
+    create_course_page.image_upload_widget.check_visible(is_image_uploaded=True)
     create_course_page.fill_create_course_form(
         title="Playwright", estimated_time="2 weeks", description="Playwright", max_score="100", min_score="10"
     )
